0696095e94b83a8ffb616866dd1d5649393b6109
nikolas <nikosnick13@live.com>
2025-12-24 10:59:32 +0200

    feat: add mull app

---
+++ src/gr/aueb/cf/testgit/MullApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class MullApp {
+
+    public static void main(String[] args) {
+        System.out.println(multiplay(12,7));
+    }
+
+    public static int multiplay(int a, int b){
+        return a * b;
+    }
+}
