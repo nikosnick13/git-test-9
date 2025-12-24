fcf673430ba4ebd8200297f29a3d1e41326dd2e7
nikolas <nikosnick13@live.com>
2025-12-24 13:30:01 +0200

    add modapp

---
+++ src/gr/aueb/cf/testgit/ModApp.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.testgit;
+
+public class ModApp {
+
+    public static void main(String[] args) {
+        System.out.println(mod(100,45));
+    }
+    public static int mod(int a, int b){
+
+        return a % b;
+    }
+}
+
+
