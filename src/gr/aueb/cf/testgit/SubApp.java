652b2ffc7f6b6e2a2ca443544444d4401ad4fd61
nikolas <nikosnick13@live.com>
2025-12-29 10:17:44 +0200

    fech:multiplay and subtraction

---
+++ src/gr/aueb/cf/testgit/SubApp.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.testgit;
+
+public class SubApp {
+    public static void main(String[] args) {
+
+        System.out.println( subb(3,5));
+    }
+    public static int subb(int a, int b){
+
+        return a - b;
+    }
+}
+
+
